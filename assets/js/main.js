d2adc8f12ff3471ab3ef36b2b6de517ceba2bfa4
GhostFA11 <hnhan201551@gmail.com>
2023-08-07 16:20:40 +0700

    scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,11 +39,79 @@ const shadowHeader = () => {
 window.addEventListener("scroll", shadowHeader);
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById("contact-form"),
+    contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    // serviceID - templateID - #form - publicKey
+    emailjs
+        .sendForm(
+            "service_t72cjhc",
+            "template_5fni8et",
+            "#contact-form",
+            "iRALvnJoixTzz5Vzf"
+        )
+        .then(
+            () => {
+                // Show sent message
+                contactMessage.textContent = "Message sent successfully ✅";
+
+                // Remove message after five seconds
+                setTimeout(() => {
+                    contactMessage.textContent = "";
+                }, 5000);
+
+                // Clear input fields
+                contactForm.reset();
+            },
+            () => {
+                // Show error message
+                contactMessage.textContent =
+                    "Message not sent (service error) ❌";
+            }
+        );
+};
+
+contactForm.addEventListener("submit", sendEmail);
 
 /*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+    const scrollUp = document.getElementById("scroll-up");
+
+    // When the scroll is higher than 350vh, add the show-scroll class to the tag with the scrollup class
+    this.scrollY >= 350
+        ? scrollUp.classList.add("show-scroll")
+        : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
 
+const scrollActive = () => {
+    const scrollDown = window.scrollY;
+
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute("id"),
+            sectionsClass = document.querySelector(
+                ".nav__menu a[href*=" + sectionId + "]"
+            );
+
+        if (
+            scrollDown > sectionTop &&
+            scrollDown <= sectionTop + sectionHeight
+        ) {
+            sectionsClass.classList.add("active-link");
+        } else {
+            sectionsClass.classList.remove("active-link");
+        }
+    });
+};
+window.addEventListener("scroll", scrollActive);
 /*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
